9cf96dd67114f64050bd9a099cc3473fcefa7257
antonionoronha <antonio.noronhaneto@gmail.com>
2020-10-05 16:13:09 -0300

    mudando app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, Blueprint
+from DMCardAPI.DMCard.restplus import api
+from DMCardAPI.DMCard.endpoints.solicitation import ns_solicitation
+from DMCardAPI.DMCard.db import config_db
+from DMCardAPI.DMCard.log import log
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app)
+
+def initialize_app(app):
+    app.config['RESTPLUS_VALIDATE'] = True
+    app.config['ERROR_404_HELP'] = False
+    app.config['CORS_HEADERS'] = 'Content-Type'
+
+    config_db(app)
+    blueprint = Blueprint('api', __name__)
+    CORS(blueprint)
+    api.init_app(blueprint)
+    app.register_blueprint(blueprint)
+
+    api.add_namespace(ns_solicitation)
+
+
+def main():
+    initialize_app(app)
+    log.info(
+        '>>>>> Starting development server at http://{}/ <<<<<'.format(app.config['SERVER_NAME']))
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
